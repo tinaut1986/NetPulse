506d1222ec26945c8c915604032e3b5cc7e6c81e
tinaut1986 <tinaut1986@gmail.com>
2026-02-28 01:25:40 +0100

    Build: Bump version to 1.2.0
    - Increment versionCode to 5 and update versionName to 1.2.0.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.tinaut1986.netpulse"
         minSdk = 28
         targetSdk = 36
-        versionCode = 4
-        versionName = "1.1.0"
+        versionCode = 5
+        versionName = "1.2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
